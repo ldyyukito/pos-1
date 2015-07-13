6257ce55f148f4d650096fda050bbe806a69950e
Zhou Yixing <sialvsic@outlook.com>
2015-07-13 23:48:20 +0800

    finish pos_v0

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,33 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
+  var result = '***<没钱赚商店>收据***\n';
+  var sum = {price: 0};
+
+  for (var i = 0; i < inputs.length; i++) {
+    result += processItem(inputs[i], sum);
+  }
+  showReceipt(result, sum);
+
+}
+
+function processItem(item, receipt) {
+
+  var count = item.count * item.price;
+  count = count.toFixed(2);
+  var temp = item.price;
+  temp = temp.toFixed(2);
+  receipt.price += parseInt(count);
+  return '名称：' + item.name + '，' + '数量：' + item.count + item.unit + '，' + '单价：' + temp + '(元)，' + '小计：' + count + '(元)' + '\n';
+
+}
+
+
+function showReceipt(result, sum) {
+
+  sum.price = sum.price.toFixed(2);
+  result += '----------------------\n';
+  result += '总计：' + sum.price + '(元)\n';
+  result += '**********************';
+  console.log(result);
 
 }
